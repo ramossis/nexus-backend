4e758989ec8b51b67c23150c03361c5b0414c3e7
RamosDev <rangerskiper@gmail.com>
2026-01-02 20:50:32 -0400

    credentials-validate

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from "cors";
 import prisma from './src/config/db.js';
 import authuser from "./src/routes/authuser.routes.js";
 const app=express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/auth',authuser)
 
--- src/controllers/authUsers.controller.js
+++ src/controllers/authUsers.controller.js
@@ -18,8 +18,26 @@ export const register=async(req,res)=>{
             }
         })
         res.status(201).json({message:"Usuario registrado con exsito",userId:user.id})
+    
+    } catch (error) {
+        // console.log(error)
+        res.status(500).json({error: error.message})
+    }
+}
+export const login=async(req,res)=>{
+    try {
+        const{email,password}=req.body
+        const user= await prisma.user.findUnique({where:{email}})
+        if(!user){
+            return res.status(400).json({message:"Credenciales Invalidas"})
+        }
+        const isMatch= await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(400).json({message:"Crendenciales Invalidas"})
+        }
+        res.status(200).json({message:"Bienvenido",user:{name:user.name}})
         
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({error:error.message})
     }
 }
